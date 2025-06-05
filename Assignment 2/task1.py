a299aaca2366f37e4a6fbc5c6ec9de9f684f5ca8
Charan Vaidya <137619080+VaidyaCharan@users.noreply.github.com>
2025-06-05 09:07:27 +0530

    Assignment 2

---
+++ Assignment 2/task1.py
@@ -0,0 +1,6 @@
+numberinput = int(input("Enter a number: "))
+
+if numberinput % 2 == 0:
+    print(f"{numberinput} is an Even number")
+else:
+    print(f"{numberinput} is an Odd number")
---
+++ Assignment 2/task2.py
@@ -0,0 +1,6 @@
+count = 0
+
+for x in range(1,51):
+    count += x
+
+print(f"The sum of numbers from 1 to 50 is: {count}")
